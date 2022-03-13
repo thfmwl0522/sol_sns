3366461c51560d5f5876292af77d11396cf7784a
solzi <thfdmlsns@naver.com>
2022-03-13 22:50:40 +0900

    login redux 수정

--- pages/signup.js
+++ pages/signup.js
@@ -1,12 +1,15 @@
 import { Button, Checkbox, Form, Input } from 'antd'
 import App from 'next/app'
 import Head from 'next/head'
-import React, { useCallback, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import AppLayout from '../components/AppLayout'
 import useInput from '../hooks/useInput'
+import { SIGN_UP } from '../reducer/User'
 import { ContentWrap, FormContent, FormInput, FormTitle, FormWrap, SignupTitle } from '../styles/components/pages/singup'
 
 const signup = () => {
+	const dispatch = useDispatch();
 	const [id, onChangeId] = useInput('');
 	const [nickname, onChangeNickname] = useInput('');
 	const [password, onChangePassword] = useInput('');
@@ -15,6 +18,14 @@ const signup = () => {
 	const [term, setTerm] = useState(false);
 	const [passwordError, setPasswordError] = useState(false);
 	const [termError, setTermError] = useState(false);
+	const { me,  } = useSelector((state) => state.user);
+
+	useEffect(() =>{
+		if (me) {
+			alert('로그인에 성공하였습니다. 메인페이지로 이동합니다.')
+			Router.push('/');
+		}
+	}, [me && me.id]);
 
 	const onChangePasswordCheck = useCallback((e) => {
 		setPasswordCheck(e.target.value !== password);
@@ -33,7 +44,15 @@ const signup = () => {
 		if (!term) {
 			return setTermError(true);
 		}
-	},[password, passwordCheck, term]);
+		return dispatch({
+			type: SIGN_UP,
+			data: {
+				email,
+				password,
+				nick
+			}
+		});
+	},[email, password, passwordCheck, term]);
 
 	return (
 		<AppLayout type="signup">
@@ -58,13 +77,13 @@ const signup = () => {
 					<FormContent>
 						<FormTitle>비밀번호 체크</FormTitle>
 						<FormInput name='passwordCheck' value={passwordCheck} type="password" required onChange={onChangePasswordCheck} />
-						{passwordError && <div>비밀번호가 일치하지 않습니다.</div>}
+						{passwordError && <div style={{ color: 'red'}}>비밀번호가 일치하지 않습니다.</div>}
 					</FormContent>
 					<FormContent $type="check">
 						<Checkbox name="user-term" checked={term} onChange={onChangeTerm}>회원가입에 동의합니다.</Checkbox>
 						{termError && <div style={{ color: 'red' }}>약관에 동의하셔야 합니다.</div>}
 					</FormContent>
-					<Button style={{width: '55%', height: '7%'}}>가입하기</Button>
+					<Button type='primary' htmlType='submit' style={{width: '55%', height: '7%', marginTop: '1rem'}}>가입하기</Button>
 				</ContentWrap>
 			</FormWrap>
 		</AppLayout>
